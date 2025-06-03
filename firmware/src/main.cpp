8f7b6ba85d8c9bd4c3681182cc4958c0abf5d2ee
Samuel Rounce <me@samuelrounce.co.uk>
2025-06-03 17:04:14 +0100

    feat: Support ESP32S3 DevkitM1

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,11 +1,66 @@
 #include <Arduino.h>
 #include <BleGamepad.h>
+#include <map>
 
-#define SHIFTER_L_GPIO 22
-#define SHIFTER_R_GPIO 23
+using namespace std;
 
 #define GAMEPAD_BUTTON_SHIFTER_L BUTTON_1
 #define GAMEPAD_BUTTON_SHIFTER_R BUTTON_2
+#define GAMEPAD_BUTTON_1         BUTTON_3
+#define GAMEPAD_BUTTON_2         BUTTON_4
+#define GAMEPAD_BUTTON_3         BUTTON_5
+#define GAMEPAD_BUTTON_4         BUTTON_6
+#define GAMEPAD_BUTTON_5         BUTTON_7
+#define GAMEPAD_BUTTON_6         BUTTON_8
+#define GAMEPAD_BUTTON_7         BUTTON_9
+#define GAMEPAD_BUTTON_8         BUTTON_10
+#define GAMEPAD_BUTTON_9         BUTTON_11
+#define GAMEPAD_BUTTON_10        BUTTON_12
+
+#ifdef GBP_DEVICE_TYPE_ESP32S_18650
+#define SHIFTER_L_GPIO GPIO_NUM_22
+#define SHIFTER_R_GPIO GPIO_NUM_23
+#define BUTTON_1_GPIO  GPIO_NUM_21
+#define BUTTON_2_GPIO  GPIO_NUM_19
+#define BUTTON_3_GPIO  GPIO_NUM_18
+#define BUTTON_4_GPIO  GPIO_NUM_17
+#define BUTTON_5_GPIO  GPIO_NUM_4
+#define BUTTON_6_GPIO  GPIO_NUM_32
+#define BUTTON_7_GPIO  GPIO_NUM_33
+#define BUTTON_8_GPIO  GPIO_NUM_25
+#define BUTTON_9_GPIO  GPIO_NUM_26
+#define BUTTON_10_GPIO GPIO_NUM_27
+#endif
+
+#ifdef GBP_DEVICE_TYPE_ESP32_S3_DEVKITM_1
+#define SHIFTER_L_GPIO GPIO_NUM_47
+#define SHIFTER_R_GPIO GPIO_NUM_48
+#define BUTTON_1_GPIO  GPIO_NUM_5
+#define BUTTON_2_GPIO  GPIO_NUM_6
+#define BUTTON_3_GPIO  GPIO_NUM_7
+#define BUTTON_4_GPIO  GPIO_NUM_15
+#define BUTTON_5_GPIO  GPIO_NUM_16
+#define BUTTON_6_GPIO  GPIO_NUM_17
+#define BUTTON_7_GPIO  GPIO_NUM_18
+#define BUTTON_8_GPIO  GPIO_NUM_8
+#define BUTTON_9_GPIO  GPIO_NUM_9
+#define BUTTON_10_GPIO GPIO_NUM_10
+#endif
+
+std::map<int, int> buttonBindings = {
+  { BUTTON_1_GPIO, BUTTON_1 },
+  { BUTTON_2_GPIO, BUTTON_2 },
+  { BUTTON_3_GPIO, BUTTON_3 },
+  { BUTTON_4_GPIO, BUTTON_4 },
+  { BUTTON_5_GPIO, BUTTON_5 },
+  { BUTTON_6_GPIO, BUTTON_6 },
+  { BUTTON_7_GPIO, BUTTON_7 },
+  { BUTTON_8_GPIO, BUTTON_8 },
+  { BUTTON_9_GPIO, BUTTON_9 },
+  { BUTTON_10_GPIO, BUTTON_10 },
+  { SHIFTER_L_GPIO, BUTTON_11 },
+  { SHIFTER_R_GPIO, BUTTON_12 },
+};
 
 BleGamepad bleGamepad("GenericButtonPlate", "srounce");
 
@@ -22,7 +77,7 @@ void setup() {
   BleGamepadConfiguration gamepadConfig;
   gamepadConfig.setControllerType(CONTROLLER_TYPE_JOYSTICK);
   gamepadConfig.setAutoReport(false);
-  gamepadConfig.setButtonCount(2);
+  gamepadConfig.setButtonCount(buttonBindings.size());
   gamepadConfig.setHatSwitchCount(0);
   gamepadConfig.setWhichAxes(false, false, false, false, false, false, false, false);
   gamepadConfig.setWhichSpecialButtons(false, false, false, false, false, false, false, false);
@@ -31,8 +86,9 @@ void setup() {
   gamepadConfig.setVid(0x5411);
   gamepadConfig.setPid(0x6969);
   
-  pinMode(SHIFTER_L_GPIO, INPUT_PULLUP);
-  pinMode(SHIFTER_R_GPIO, INPUT_PULLUP);
+  for (const auto &binding : buttonBindings) {
+    pinMode(binding.first, INPUT_PULLUP);
+  }
 
   bleGamepad.begin(&gamepadConfig);
 }
@@ -43,15 +99,13 @@ void loop() {
     batteryLevel = getBatteryPercent();
     bleGamepad.setBatteryLevel(batteryLevel);
     
-    if(!digitalRead(SHIFTER_L_GPIO)) 
-      bleGamepad.press(GAMEPAD_BUTTON_SHIFTER_L);
-    else
-      bleGamepad.release(GAMEPAD_BUTTON_SHIFTER_L);
-  
-    if(!digitalRead(SHIFTER_R_GPIO)) 
-      bleGamepad.press(GAMEPAD_BUTTON_SHIFTER_R);
-    else
-      bleGamepad.release(GAMEPAD_BUTTON_SHIFTER_R);
+    for (const auto &binding : buttonBindings) {
+      if(!digitalRead(binding.first)) {
+        bleGamepad.press(binding.second);
+      } else {
+        bleGamepad.release(binding.second);
+      }
+    }
     
     bleGamepad.sendReport();
     delay(5);
